466214b0b2bab7bec17d8436f2ec0a4f2ac0d7bf
lottenoorlander <lottenoorlander@gmail.com>
2020-01-09 17:21:04 +0100

    login works for both buyers and sellers

--- src/auth/router.js
+++ src/auth/router.js
@@ -8,20 +8,50 @@ const Seller = require("../seller/model");
 router.post("/login", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  const sellerOrBuyer = req.body.sellerOrBuyer;
+  const isSeller = req.body.isSeller;
 
   if (!username || !password) {
     rses.status(400).send({
       error_code: 3,
       message: "Please supply a valid username and password"
     });
-  } else {
-    [sellerOrBuyer]
-      .findOne({
-        where: {
-          username: req.body.username
+  } else if (isSeller) {
+    Seller.findOne({
+      where: {
+        username: req.body.username
+      }
+    })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            error_code: 4,
+            message: "User with that username and password does not exist"
+          });
+        } else if (bcrypt.compareSync(req.body.password, entity.password)) {
+          res.send({
+            jwt: toJWT({ userId: entity.id }),
+            user: entity.username
+          });
+        } else {
+          res.status(400).send({
+            error_code: 4,
+            message: "User with that username and password does not exist"
+          });
         }
       })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send({
+          error_code: 0,
+          message: "Something went wrong with the server"
+        });
+      });
+  } else {
+    Buyer.findOne({
+      where: {
+        username: req.body.username
+      }
+    })
       .then(entity => {
         if (!entity) {
           res.status(400).send({
